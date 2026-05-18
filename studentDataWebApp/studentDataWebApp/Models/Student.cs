a935327b87d6ff8e2ec66c8abc815fc02e064ff8
professorRED09 <sittikornworks01@gmail.com>
2026-05-18 11:23:37 +0700

    [Add] student class model

---
+++ studentDataWebApp/studentDataWebApp/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace studentDataWebApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Honorifics { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string Faculty { get; set; }
+    }
+}
