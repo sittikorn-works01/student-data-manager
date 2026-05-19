94f2a732db66f949b68f454c06eaf7a841655a30
professorRED09 <sittikornworks01@gmail.com>
2026-05-19 20:54:23 +0700

    [Mod] add studentID column for student model

--- studentDataWebApp/studentDataWebApp/Models/Student.cs
+++ studentDataWebApp/studentDataWebApp/Models/Student.cs
@@ -1,12 +1,29 @@
-﻿namespace studentDataWebApp.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace studentDataWebApp.Models
 {
     public class Student
     {
         public int Id { get; set; }
+
+        [Required]
+        [DisplayName("รหัสนักศึกษา")]
+        public string StudentId { get; set; }
+        [Required]
+        [DisplayName("คำนำหน้า")]
         public string Honorifics { get; set; }
+        [Required]
+        [DisplayName("ชื่อ")]
         public string FirstName { get; set; }
+        [Required]
+        [DisplayName("นามสกุล")]
         public string LastName { get; set; }
+        [Required]
+        [DisplayName("วันเดือนปีเกิด")]
         public DateOnly DateOfBirth { get; set; }
+        [Required]
+        [DisplayName("สำนักวิชา")]
         public string Faculty { get; set; }
     }
-}
+}   
