c00a8f93a89e7dfcf1a0e0c31087ea90ea5cc9f0
professorRED09 <sittikornworks01@gmail.com>
2026-05-19 20:55:13 +0700

    [Mod] add Create, Edit student data

--- studentDataWebApp/studentDataWebApp/Controllers/StudentController.cs
+++ studentDataWebApp/studentDataWebApp/Controllers/StudentController.cs
@@ -1,12 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using studentDataWebApp.Data;
+using studentDataWebApp.Models;
 
 namespace studentDataWebApp.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly ApplicationDBContext _db;
+        public StudentController(ApplicationDBContext db)
+        {
+            this._db = db;
+        }
         public IActionResult Index()
         {
+            IEnumerable<Student> allStudent = _db.Students;
+            return View(allStudent);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }   
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Student newStudent)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Students.Add(newStudent);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(newStudent);            
+        }
+
+        public IActionResult Edit(int? id) {
+            if (id == null || id == 0) return NotFound();
+            
+            var obj = _db.Students.Find(id);
+            if (obj == null) return NotFound();
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Student editedStudent)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Students.Update(editedStudent);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(editedStudent);
         }
     }
 }
