3d3468c92ce3103ba149ae5173f4a7a78c0ac2c7
professorRED09 <sittikornworks01@gmail.com>
2026-05-26 11:50:43 +0700

    [Add] add migration and update database for new honorific input

--- studentDataWebApp/studentDataWebApp/Migrations/20260525155132_AddHonorificEnum.Designer.cs
+++ studentDataWebApp/studentDataWebApp/Migrations/20260525155132_AddHonorificEnum.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using studentDataWebApp.Data;
 
@@ -11,9 +12,11 @@ using studentDataWebApp.Data;
 namespace studentDataWebApp.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260525155132_AddHonorificEnum")]
+    partial class AddHonorificEnum
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
